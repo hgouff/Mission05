eb0d53e78559a84a1c7c3e9d86f89cd8411c8f39
Hannah Gouff <hannahgouff@gmail.com>
2025-02-05 22:47:59 -0700

    commit

--- Mission05/obj/Debug/net8.0/Mission05.AssemblyInfo.cs
+++ Mission05/obj/Debug/net8.0/Mission05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c993574c8b050577a4d31b44f15e86f857d6eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
